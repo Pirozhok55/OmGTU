016ab1f4a22d420441f62746f53c160fcba611b5
Pirozhok55 <alx444bp@gmail.com>
2023-11-13 10:23:13 +0600

    +rep

---
+++ Algoritmiz/LabRabClass/11.11/11.11/Program.cs
@@ -0,0 +1,107 @@
+﻿using System;
+namespace _1123
+{
+    internal class Program
+    {
+        static int[,] SwapAll(int[,] massiv)
+        {
+            int[,] swapMas = new int[massiv.GetLength(0), massiv.GetLength(1)];
+            for (int i = 0; i < massiv.GetLength(0); i++)
+            {
+                for (int j = 0; j < (massiv.GetLength(1) / 2) * 2; j++)
+                {
+                    if (j % 2 == 0) swapMas[i, j] = massiv[i, (j + 1)];
+                    else swapMas[i, j] = massiv[i, (j - 1)];
+                }
+            }
+            if ((massiv.GetLength(1) / 2) * 2 != massiv.GetLength(1))
+            {
+                for (int i = 0; i < massiv.GetLength(0); i++) swapMas[i, massiv.GetLength(1) - 1] = massiv[i, massiv.GetLength(1) - 1];
+            }
+            return swapMas;
+        }
+
+        static int[] NotNullInString(int[,] massiv)
+        {
+            int[] notNullElems = new int[massiv.GetLength(1)];
+            for (int i = 0; i < massiv.GetLength(1); i++)
+            {
+                int sum = 0;
+                for (int j = 0; j < massiv.GetLength(0); j++)
+                {
+                    if (massiv[j, i] != 0) sum++;
+                }
+                notNullElems[i] = sum;
+            }
+            return notNullElems;
+        }
+
+        static string[] MaxInString(int[,] massiv)
+        {
+            int maxVal = int.MinValue;
+            string[] maxMas = new string[massiv.GetLength(0)];
+            for (int j = 0; j < massiv.GetLength(0); j++)
+            {
+                bool det = false;
+                for (int i = 0; i < massiv.GetLength(1); i++)
+                {
+                    if (maxVal < massiv[j, i] && massiv[j, i] % 2 == 0)
+                    {
+                        maxMas[j] = massiv[j, i].ToString();
+                        det = true;
+                    }
+                }
+                if (det == false) maxMas[j] = "NoEl";
+            }
+            return maxMas;
+        }
+        static int[,] MassiveGenerate(string n, string m)
+        {
+            int[,] mainMassive = new int[int.Parse(n), int.Parse(m)];
+            for (int i = 0; i < int.Parse(n); i++)
+            {
+                Console.Write("Ввод " + (i + 1) + " строки: ");
+                string[] strMas = Console.ReadLine().Split();
+                for (int j = 0; j < int.Parse(m); j++)
+                {
+                    mainMassive[i, j] = int.Parse(strMas[j]);
+                }
+            }
+            return mainMassive;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ввод размера массива: ");
+            string[] nm = Console.ReadLine().Split();
+            int[,] mainMassive = MassiveGenerate(nm[0], nm[1]);
+            string[] maxMas = MaxInString(mainMassive);
+            int[] notNull = NotNullInString(mainMassive);
+
+            Console.Write("\n1. Массив максимальных четных по строкам: ");
+            foreach (string i in maxMas)
+            {
+                Console.Write(i + " ");
+            }
+            Console.WriteLine();
+
+            Console.Write("2. Массив кол-ва ненулевых эл-в по столбцам: ");
+            foreach (int i in notNull)
+            {
+                Console.Write(i + " ");
+            }
+            Console.WriteLine();
+
+            Console.Write("3. ");
+            int[,] Swap = SwapAll(mainMassive);
+            for (int i = 0; i < Swap.GetLength(0); i++)
+            {
+                Console.Write("\t{");
+                for (int j = 0; j < Swap.GetLength(1); j++) Console.Write('\t' + Swap[i, j].ToString() + " ");
+                Console.WriteLine("\t}");
+            }
+
+            Console.Read();
+        }
+    }
+}
---
+++ Algoritmiz/LabRabClass/14.10/14.10/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _14._10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.Clear();
+
+
+                Console.Write("Введите количество строк: ");
+                int n = int.Parse(Console.ReadLine());
+                Console.Write("Введите количество столбцов: ");
+                int m = int.Parse(Console.ReadLine());
+                int kratfive = 0, sumel = 0, proizel = 1, sumbol = 0, minel = 2147483647, proizchet = 1, kol4 = 0;
+
+                int[,] massive = new int[n, m];
+                Console.WriteLine("Ввод элементов массива: ");
+                for (int i = 0; i < n; i++)
+                {
+                    for (int j = 0; j < m; j++)
+                    {
+                        massive[i, j] = int.Parse(Console.ReadLine());
+                        /*1*/
+                        if (massive[i, j] % 5 == 0) kratfive++;
+                        /*2*/
+                        sumel += massive[i, j];
+                        proizel *= massive[i, j];
+                        /*4*/
+                        if (massive[i, j] >= 0 && massive[i, j] % 2 == 0)
+                        {
+                            proizchet *= massive[i, j];
+                            kol4++;
+                        }
+                    }
+                    if (sumel > proizel) sumbol++;
+                    sumel = 0;
+                    proizel = 1;
+                }
+                
+
+
+/*1*/           Console.WriteLine("1. Количество элементов кратных 5: " + kratfive);
+/*2*/           Console.WriteLine("2. Количество строк с суммой эл-в больше произведения: " + sumbol);
+
+/*3*/           Console.WriteLine("3. Минимальные элементы в каждом столбце: ");
+                for (int i = 0; i < m; i++)
+                {
+                    for (int j = 0; j < n; j++)
+                    {
+                        if (massive[j, i] < minel)minel = massive[j,i];
+                    }
+                    Console.WriteLine("В " + i + "м столбце: " + minel);
+                    minel = 2147483647;
+                }
+/*4*/           if(kol4 > 0) Console.WriteLine("4. Произведение неотриц-х четных эл-в: " + proizchet);
+                else Console.WriteLine("4. Неотриц-е четные отсутствуют.");
+
+                int[,] mas = new int[n,3];
+/*5*/           int proiz5 = 1, sum5 = 0, kol0 = 0, otvet5 = -n;
+                for(int i = 0; i < n; i++)
+                {
+                    for(int j = 0; j < m; j++)
+                    {
+                        proiz5 *= massive[i, j];
+                        sum5 += massive[i, j];
+                        if (massive[i, j] == 0) kol0++;
+                    }
+                    mas[i, 0] = proiz5;
+                    mas[i, 1] = sum5;
+                    mas[i, 2] = kol0;
+                    proiz5 = 1;
+                    sum5 = 0;
+                }
+                for (int i = 0; i < n; i++)
+                {
+                    for(int k = i; k < n; k++)
+                    {
+                        if (mas[i, 0] == mas[k, 0] && mas[i, 1] == mas[k, 1] && mas[i, 2] == mas[k, 2]) otvet5++; 
+                    }
+                    
+                }
+                Console.WriteLine("5. Количество пар строк с одинаковыми элементами: " + otvet5);
+
+                Console.ReadLine();
+            }
+        }
+    }
+}
